d99b2826abc51ced8d4622d9c0db084ce90d642a
Bareera Qureishi <127762940+Bareeraq@users.noreply.github.com>
2026-02-13 00:34:08 +0530

    modified:   auth.js 	modified:   home.html 	modified:   index.html

--- auth.js
+++ auth.js
@@ -1,14 +1,11 @@
-    // const oktaAuth = new OktaAuth({
-    //     issuer: "https://inadev-integrator-9091209.okta.com",
-    //     clientId: "0oazzri91lyvXY9DV697",
-    //     redirectUri: "http://localhost:5500/home.html",
-    //     scopes: ["openid", "profile", "email"]
-    // });
-const oktaAuth = new OktaAuth ({
+const oktaAuth = new OktaAuth({
     issuer: "https://integrator-9091209.okta.com/oauth2/default",
     clientId: "0oa1024dsa3zzVfbC698",
     redirectUri: "http://localhost:5500/home.html",
-    scopes: ["openid", "profile", "email"]
+    scopes: ["openid", "profile", "email"],
+    tokenManager: {
+    storage: 'localStorage'
+  }
 });
 function attachLoginHandler() {
     const loginBtn = document.getElementById('loginBtn');
@@ -22,4 +19,11 @@ function attachLoginHandler() {
             }
         });
     }
-}       
+}
+
+// const oktaAuth = new OktaAuth({
+//     issuer: "https://inadev-integrator-9091209.okta.com",
+//     clientId: "0oazzri91lyvXY9DV697",
+//     redirectUri: "http://localhost:5500/home.html",
+//     scopes: ["openid", "profile", "email"]
+// });
